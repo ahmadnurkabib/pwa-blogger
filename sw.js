fef33acd3bfc3541a1fe2a37bcdaac7d3ee23e49
AHMAD NUR KABIB <ahmadenterpreneur@gmail.com>
2026-07-27 22:32:55 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,11 @@
+const CACHE_NAME = 'ahmadservicecenter-v1';
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
